6e1b513219087e2aeafef0b9df0d1d93c47d4bde
Colton Crum <30506411+crumion@users.noreply.github.com>
2021-07-23 17:12:19 -0400

    Delete Feature 4 directory

--- Feature 4/my-app/src/environments.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  PARSE_APPLICATION_ID: "dYrqBUsZJ2o06REnwa356VBbzHpDR0M4A3DDjDUc",
-  PARSE_HOST_URL: "https://parseapi.back4app.com/",
-  PARSE_JAVASCRIPT_KEY: "Ci8bDH2stxRvu1RIIqr2PlEdTkPndiTGjiwFIwlO"
-};
