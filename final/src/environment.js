c559b2118c95746397800c4c9232c3485f670c33
Colton Crum <30506411+crumion@users.noreply.github.com>
2021-08-09 22:23:21 -0400

    Delete final directory

--- final/src/environment.js
+++
@@ -1,7 +0,0 @@
-// This folder exports all of the necessary keys and IDs necessary for the project
-
-module.exports = {
-  APPLICATION_ID: "OaC0VMsA7qJj7dEBdfPpBlB62iqEk0ldmXy7thBm",
-  JAVASCRIPT_KEY: "7oatg3gVRA8va15DbDKPaH1pNfpzJJc4Rhu2aNH4",
-  SERVER_URL: "https://parseapi.back4app.com/"
-};
