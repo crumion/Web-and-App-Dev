05559a440da526ebbc725f764d142d4a40b1dfa5
Colton Crum <30506411+crumion@users.noreply.github.com>
2021-08-09 22:22:14 -0400

    Delete Feature3 directory

--- Feature3/services/info.js
+++
@@ -1,8 +0,0 @@
-/* The function we need to grab the information from the JSON file */
-/* We used axios specifically to do this, in the same manner as mwicks' function */
-export function getInfos() {
-  const axios = window.axios;
-  return axios.get("./services/user.json").then((response) => {
-    return response.data.data;
-  });
-}
